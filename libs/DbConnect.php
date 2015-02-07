4d173cca40cd43414a0c5f36a03956b2e9bd8a49
rjsanchez <rjsanchezestevez@gmail.com>
2015-02-07 22:13:48 +0100

    Versión original

---
+++ libs/DbConnect.php
@@ -0,0 +1,15 @@
+<?php
+$host = "raccoonweb.ddns.net";
+$db_name = "practica";
+$username = "root";
+$password = "root";
+
+try {
+	$con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+}
+
+//to handle connection error
+catch(PDOException $exception){
+	echo "Connection error: " . $exception->getMessage();
+}
+?>
