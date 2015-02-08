9c2bbbdaa0da138a08590647e2439e88450a5e01
rjsanchez <rjsanchezestevez@gmail.com>
2015-02-08 19:22:37 +0100

    refactor UserLogin file

--- src/Classes/UserLogin.php
+++ src/Classes/UserLogin.php
@@ -1,67 +1,81 @@
 <?php
-// form is submitted, check if acess will be granted
-if($_POST){
 
-    try{
-        // load database connection and password hasher library
-        require 'DbConnect.php';
-        require 'PasswordHash.php';
+    require 'DbConnect.php';
+    require 'PasswordHash.php';
 
-        // prepare query
+    $isPostRequest =  $_SERVER['REQUEST_METHOD'] != 'POST';
+    $dbConection =  $con;
+
+    if($isPostRequest){
+        echo ACCESS_DENIED;
+    }
+    const ACCESS_GRANTED =   "<div>Access granted.</div>";
+    const ACCESS_DENIED =   "<div>Access denied. <a href='login.php'>Back.</a></div>";
+
+
+    $formUserMail = $_POST['email'];
+    $formUserPassword = $_POST['password'];
+
+    if(!userExists($formUserMail, $dbConection )){
+         echo ACCESS_DENIED;
+         exit();
+    }
+
+    $user = getUserFromDatabase($formUserMail, $dbConection );
+    $userPasswordStored = $user["password"];
+    $isValidUser = checkLogin($formUserPassword, $userPasswordStored);
+
+
+    if(!$isValidUser){
+        echo ACCESS_DENIED;
+        exit();
+    }
+
+    echo ACCESS_GRANTED;
+
+
+
+     function userExists($userMail, $dbConection )
+     {
+
+        $stmt = findUser($userMail, $dbConection );
+        return $stmt->rowCount();
+
+    }
+
+     function findUser($formUserMail, $con)
+     {
+
         $query = "select email, password from users where email = ? limit 0,1";
         $stmt = $con->prepare( $query );
 
         // this will represent the first question mark
-        $stmt->bindParam(1, $_POST['email']);
-
-        // execute our query
+        $stmt->bindParam(1, $formUserMail);
         $stmt->execute();
 
-        // count the rows returned
-        $num = $stmt->rowCount();
-
-        if($num==1){
-
-            //store retrieved row to a 'row' variable
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            // hashed password saved in the database
-            $storedPassword = $row['password'];
-
-            // salt and entered password by the user
-            $salt = "ilovecodeofaninjabymikedalisay";
-            $postedPassword = $_POST['password'];
-            $saltedPostedPassword = $salt . $postedPassword;
-
-            // instantiate PasswordHash to check if it is a valid password
-            $hasher = new PasswordHash(8,false);
-            $check = $hasher->CheckPassword($saltedPostedPassword, $storedPassword);
-
-            /*
-             * access granted, for the next steps,
-             * you may use my php login script with php sessions tutorial :)
-             */
-            if($check){
-                echo "<div>Access granted.</div>";
-            }
-
-            // $check variable is false, access denied.
-            else{
-                echo "<div>Access denied. <a href='login.php'>Back.</a></div>";
-            }
-
-        }
-
-        // no rows returned, access denied
-        else{
-            echo "<div>Access denied. <a href='login.php'>Back.</a></div>";
-        }
+        return $stmt;
 
     }
-        //to handle error
-    catch(PDOException $exception){
-        echo "Error: " . $exception->getMessage();
+
+    function getUserFromDatabase($formUserMail, $dbConection )
+    {
+        $stmt = findUser($formUserMail, $dbConection );
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
+    function checkLogin($formUserPassword, $userPasswordStored)
+    {
 
-}
+        // salt and entered password by the user
+        $salt = "ilovecodeofaninjabymikedalisay";
+
+        $saltedPostedPassword = $salt . $formUserPassword;
+
+        // instantiate PasswordHash to check if it is a valid password
+        $hasher = new PasswordHash(8,false);
+        $check = $hasher->CheckPassword($saltedPostedPassword, $userPasswordStored);
+
+        return $check;
+
+    }
+
