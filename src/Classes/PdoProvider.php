ca51ee117ff30ca356bf8a8b76d13171e3d7bea2
Ricardo Sánchez <rjsanchezestevez@gmail.com>
2015-02-14 19:33:53 +0100

    Modificacion pdo provider para extender de pdo y agregar algunos metodos para acceso a base de datos, aislando la parte de persistencia

--- src/Classes/PdoProvider.php
+++ src/Classes/PdoProvider.php
@@ -4,24 +4,34 @@ namespace src\classes;
 
 final class PdoProvider extends \PDO
 {
-	private $host = 'localhost';
-	private $user = 'root';
-	private $password = 'root';
-	private $database = 'practica';
 
-	public function __construct()
+	public function __construct(Array $sql_config)
 	{
 		try {
-			parent::__construct(sprintf('mysql: host=%s; dbname=%s', $this->host, $this->database), $this->user, $this->password);
+			parent::__construct(sprintf('mysql: host=%s; dbname=%s', $sql_config["host"], $sql_config["database"]), $sql_config["user"], $sql_config["password"]);
 
-			$this->setAttribute(parent::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES UTF8');
 			$this->setAttribute(parent::ATTR_ERRMODE, parent::ERRMODE_EXCEPTION);
-			$this->setAttribute(parent::ATTR_EMULATE_PREPARES, false);
 			$this->setAttribute(parent::ATTR_DEFAULT_FETCH_MODE, parent::FETCH_ASSOC);
 
-		} catch(PDOException $e) {
+		} catch (PDOException $e) {
 
 			die($e->getMessage());
+			echo "error";
 		}
 	}
+
+	public function runQuery($query, $params){
+		$stmt = $this->prepare( $query );
+		foreach ($params as $key => &$value) {
+			$stmt->bindParam( $key, $value);
+		}
+		$stmt->execute();
+		return $stmt;
+	}
+
+	public function nextRow($stmt){
+		return $stmt->fetch(\PDO::FETCH_ASSOC);
+
+	}
+
 }
