f5253749b596718527f0ca41c60afa03fee03df7
Ricardo Sánchez <rjsanchezestevez@gmail.com>
2015-02-14 19:35:44 +0100

    Ficheros de configuración para rutas y conexion a base de datos

---
+++ config/login_params.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'redirectAfterLogin'            =>  '../../home.php',
+    'redirectErrorLogin'            =>  '../../badLogin.php',
+    'redirectAfterRegister'         =>  '../../registrationOk.php',
+    'redirectErrorRegister'         =>  '../../registrationFail.php',
+    'ConsoleMenssageAfterLogin'     =>  'Access granted.',
+    'ConsoleMenssageErrorLogin'     =>  'Access denied.',
+    'ConsoleMenssageAfterRegister'  =>  'Register Successful.',
+    'ConsoleMenssageErrorRegister'  =>  'Register fail.',
+);
+
